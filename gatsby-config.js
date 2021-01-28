6ac3d0fd5c77f89910dc4b0e6401203390fa8b03
Aadithya Sivasankar <aadithya.sivasankar@siemens.com>
2021-01-28 23:54:12 +0530

    Add pipeline changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+require("dotenv").config();
 module.exports = {
   /* Your site config here */
   plugins: [],
--- src/components/controller.js
+++ src/components/controller.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const get = (endpoint) =>
-  axios.get(`http://localhost:8080/feed/posts/${endpoint}`);
+  axios.get(`${process.env.GATSBY_API_URL}feed/posts/${endpoint}`);
 
 export const createPages = async () => {
   const movie = await get("rambo");
